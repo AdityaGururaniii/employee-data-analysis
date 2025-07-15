663ceb6874057514b89251e74c7604291bda23ae
AdityaGururaniii <adityagururanii@gmail.com>
2025-07-15 19:51:15 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-white p-6">
+      <h1 className="text-2xl font-bold">Employee Data Analysis</h1>
+      <p>Made with ❤️ by Aditya Gururani</p>
+    </div>
+  )
+}
+
+export default App
